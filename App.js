0c7c475645ac2fa75d3e763177c8cecb000d5481
MeltemGerceker <meltemy@gmail.com>
2023-06-21 16:41:48 +0300

    initialize project

---
+++ App.js
@@ -0,0 +1,36 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+function App() {
+  
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>To Do App</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
