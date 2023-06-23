4dc78e8fa01df902550b6c4bbec73c7706690e0f
MeltemGerceker <meltemy@gmail.com>
2023-06-23 17:51:44 +0300

    added EditModal component

--- src/components/todo/index.js
+++ src/components/todo/index.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import {View, Text, TouchableOpacity, Alert} from "react-native";
 import styles from "./style";
 import { colors } from "../../utils/constants";
 
 import Icon from "react-native-vector-icons/AntDesign";
+import EditModal from '../editModal';
 
 const Todo = ({todo={}, todos=[], setTodos=()=>{}}) => {
-    
+    const [openModal, setOpenModal] = useState(false);
+    const [modalText, setModalText] = useState(todo?.text);
+    const [hasError, setHasError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
+
     const deleteTodo = () => {
         Alert.alert("ALERT!", 
             `Are you sure you want to delete "${todo?.text}" ?`,
@@ -30,8 +35,7 @@ const Todo = ({todo={}, todos=[], setTodos=()=>{}}) => {
         let tempTodos = [];
         const changedTodo = {
             ...todo,
-            completed: !todo.completed,
-            deneme: 0
+            completed: !todo.completed
         };
         for (let i = 0; i < todos.length; i++) {
             if (todos[i].id !== todo.id)
@@ -41,34 +45,36 @@ const Todo = ({todo={}, todos=[], setTodos=()=>{}}) => {
         }
         //console.log(tempTodos);
         setTodos(tempTodos);
-        /*
-        Alert.alert("COMPLETE!", "Are you sure you want to change this To Do's status?",
-        [
-            {
-                text: "OK",
-                style: "default",
-                onPress: () => {
-                    let tempTodos = [];
-                    const changedTodo = {
-                        ...todo, 
-                        completed: !todo.completed,
-                        deneme: 0
-                    };
-                    for (let i = 0; i < todos.length; i++) {
-                        if (todos[i].id !== todo.id)
-                            tempTodos.push(todos[i]);
-                        else
-                            tempTodos.push(changedTodo);
-                    }
-                    setTodos(tempTodos);
-                }
-            },
-            {
-                text: "CANCEL",
-                style: "cancel"
-            },
-        ])
-        */
+    };
+
+    const editTodo = () => {
+        if (modalText === "") { // Validation
+            setHasError(true);
+            setErrorMessage("The text cannot be empty string.");
+
+            setTimeout(() => {
+                setHasError(false);
+                setErrorMessage("");
+            }, 2000);
+            return;
+        }
+
+        setHasError(false);
+        setErrorMessage("");
+
+        let tempTodos = [];
+        const changedTodo = {
+            ...todo,
+            text: modalText
+        };
+        for (let i = 0; i < todos.length; i++) {
+            if (todos[i].id !== todo.id)
+                tempTodos.push(todos[i]);
+            else
+                tempTodos.push(changedTodo);
+        }
+        setTodos(tempTodos);
+        setOpenModal(false);
     };
 
     return (
@@ -81,13 +87,22 @@ const Todo = ({todo={}, todos=[], setTodos=()=>{}}) => {
                 <TouchableOpacity onPress={toggleCompleted}>
                     <Icon name={todo?.completed ? "checkcircle" : "checkcircleo"} size={25} color={colors.checkButtonGreen}/>
                 </TouchableOpacity>
-                <TouchableOpacity>
+                <TouchableOpacity onPress={()=>{setOpenModal(true)}}>
                     <Icon name="edit" size={25} color={colors.tertiaryColor}/>
                 </TouchableOpacity>
                 <TouchableOpacity onPress={deleteTodo}>
                     <Icon name="closecircleo" size={25} color={colors.deleteButtonRed}/>
                 </TouchableOpacity>
             </View>
+            <EditModal 
+                visible={openModal} 
+                modalText={modalText} 
+                setModalText={setModalText} 
+                closeModal={() => {setOpenModal(false)}}
+                onSave={editTodo}
+                hasError={hasError}
+                errorMessage={errorMessage}
+            />
         </View>
     )
 }
